9e10f38403e1b340a6afb76131fa0e68d051be92
GermanT <tornellogerman@gmail.com>
2026-08-09 22:34:29 -0300

    Admins cambiados 3

--- backend/routes.py
+++ backend/routes.py
@@ -5,6 +5,25 @@ from models import Familia, Invitado
 
 api = Blueprint("api", __name__)
 
+@api.route("/login_admin", methods=["POST", "OPTIONS"])
+def login_admin():
+
+    if request.method == "OPTIONS":
+        return "", 200
+
+    datos = request.get_json()
+
+    password = datos.get("password")
+
+    if password == "Boda2026!":
+
+        return jsonify({
+            "mensaje": "Login correcto"
+        }), 200
+
+    return jsonify({
+        "error": "Contraseña incorrecta"
+    }), 401
 
 @api.route("/buscar/<nombre>")
 def buscar(nombre):
